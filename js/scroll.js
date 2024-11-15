79df0fc514e1922d91f1943cb10e3d7e1cd466f3
Mike Lei <mikelei8291@users.noreply.github.com>
2024-11-16 03:17:09 +0800

    fix(scroll): invoke the listener immediately after observing

--- js/scroll.js
+++ js/scroll.js
@@ -1,24 +1,27 @@
 export class ScrollObserver {
     constructor() {
         this.targets = [];
-        window.addEventListener("scroll", () => {
-            this.targets.forEach(({ element, callback, overshot, delay }) => {
-                let progress = ((window.innerHeight - element.getBoundingClientRect().top) / window.innerHeight - 1) * overshot - delay;
-                if (progress < 0) {
-                    progress = 0;
-                } else if (progress > 1) {
-                    progress = 1;
-                }
-                element.style.setProperty("--progress", progress.toString());
-                if (callback) {
-                    callback(progress);
-                }
-            });
-        }, { passive: true });
+        window.addEventListener("scroll", this.listener, { passive: true });
+    }
+
+    listener = () => {
+        this.targets.forEach(({ element, callback, overshot, delay }) => {
+            let progress = ((window.innerHeight - element.getBoundingClientRect().top) / window.innerHeight - 1) * overshot - delay;
+            if (progress < 0) {
+                progress = 0;
+            } else if (progress > 1) {
+                progress = 1;
+            }
+            element.style.setProperty("--progress", progress.toString());
+            if (callback) {
+                callback(progress);
+            }
+        });
     }
 
     observe(element, callback = null, overshot = 1, delay = 0) {
         this.targets.push({ element, callback, overshot, delay });
+        this.listener();
     }
 
     unobserve(element) {
